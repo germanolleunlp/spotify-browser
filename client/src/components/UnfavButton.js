bbbfc7088c20ef299b5542b5cc15bf13f824d3f4
machadojuanmanuel <machadojuanmanuel@hotmail.com>
2020-08-09 18:32:09 -0300

    Show confirm modal when remove from fav

--- client/src/components/UnfavButton.js
+++ client/src/components/UnfavButton.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
 import { makeStyles } from "@material-ui/core/styles";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 
 import Button from "./Button";
+import Modal from "./Modal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,16 +15,37 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const UnfavButton = () => {
+const UnfavButton = ({ track }) => {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const removeFromFavs = () => {
+    toggleModal();
+  };
 
   return (
-    <Button
-      className={classes.root}
-      figma="figma-button-display-btn-spotify-unfav"
-    >
-      <DeleteOutlineIcon />
-    </Button>
+    <Fragment>
+      <Button
+        className={classes.root}
+        figma="figma-button-display-btn-spotify-unfav"
+        onClick={toggleModal}
+      >
+        <DeleteOutlineIcon />
+      </Button>
+      <Modal
+        title="Remove from Favorites"
+        message={`¿Are you sure you want to remove "${
+          track.title
+        }" from your "Favorites" list?`}
+        handleConfirm={removeFromFavs}
+        handleClose={toggleModal}
+        isOpen={isOpen}
+      />
+    </Fragment>
   );
 };
 
