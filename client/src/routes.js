052fdf2d81b22191e9ff3414de59a40f23991a1c
machadojuanmanuel <machadojuanmanuel@hotmail.com>
2020-08-10 20:01:10 -0300

    Add fav page

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,7 @@ export const HOME_URL = "/";
 export const SEARCH_URL = "/search";
 export const AUTH_CALLBACK_URL = "/auth_callback";
 export const ARTIST_URL = "/:artistId/artist";
+export const FAVORITES_URL = "/favorites";
 export const STLYE_GUIDE_URL = "/style_guide";
 export const ERROR_URL = "/error";
 export const NOT_FOUND_URL = "/not_found";
