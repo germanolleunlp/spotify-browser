683ec6a9a8ed0717cba837b93b5cc13ee21431d3
machadojuanmanuel <machadojuanmanuel@hotmail.com>
2020-08-10 20:01:10 -0300

    Add fav page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,11 +1,15 @@
 import React from "react";
-import clsx from "clsx";
+import { connect } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 
 import Typography from "./Typography";
 import Alert from "./Alert";
+import Button from "./Button";
+
+import { FAVORITES_URL } from "../routes";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,30 +19,58 @@ const useStyles = makeStyles((theme) => ({
   default: {
     color: `${theme.palette["bg-default"].main}`,
   },
+  button: {
+    marginBottom: theme.spacing(2),
+  },
 }));
 
-const Welcome = () => {
+const Welcome = ({ favorites }) => {
   const classes = useStyles();
+  const location = useLocation();
+
+  const inFavoritesPage = location.pathname === FAVORITES_URL;
+  const showFavorites = !!Object.keys(favorites).length && !inFavoritesPage;
+
   return (
     <Box className={classes.root} data-testid="welcome">
       <Typography
         figma={"figma-typography-display-4"}
-        className={clsx(classes.default)}
+        className={classes.default}
         data-testid="welcome-title"
       >
         Looking for music?
       </Typography>
       <Typography
         figma={"figma-typography-display-4"}
-        className={clsx(classes.default)}
+        className={classes.default}
         data-testid="welcome-subtitle"
         gutterBottom
       >
         Find your favorite tracks and artists.
       </Typography>
+      {showFavorites && (
+        <Link to={FAVORITES_URL}>
+          <Button
+            figma={"figma-button-display-btn-spotify-primary"}
+            className={classes.button}
+            data-testid="favorites-link-button"
+          >
+            <Typography
+              figma={"figma-typography-text-1"}
+              className={classes.default}
+            >
+              Go to favorites
+            </Typography>
+          </Button>
+        </Link>
+      )}
       <Alert fixed />
     </Box>
   );
 };
 
-export default Welcome;
+const mapStateToProps = ({ root: { favorites } }) => ({
+  favorites,
+});
+
+export default connect(mapStateToProps)(Welcome);
